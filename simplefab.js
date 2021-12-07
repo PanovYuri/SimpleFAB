2901e2c8e0b36930a8a193eb6035ba2a117b19ec
Panov Yury <yury.panov@infotecs.ru>
2021-12-07 07:39:49 +0300

    rename classes with add support title

--- simplefab.js
+++ simplefab.js
@@ -1,31 +1,24 @@
 class SimpleFAB {
-    _menuCollection = Object()
-
     constructor(menuElement) {
-        menuElement.forEach(menu => {
-            const items = menu.querySelectorAll('.sfab__item');
-            const toggleBtn = menu.querySelector('.sfab__toggle');
+        this.menu = menuElement;
 
-            menu.style.cssText = `--items-count: ${items.length};`
-            items.forEach((item, i) => item.style.cssText = `--i:${i}`)
-            toggleBtn.addEventListener('click', () => menu.classList.toggle('active'))
+        const items = this.menu.querySelectorAll('.sfab__item');
+        const toggleBtn = this.menu.querySelector('.sfab__toggle');
 
-            // Store all menu with id in map
-            if (menu.id) {
-                this._menuCollection[menu.id] = menu
-            }
-        })
+        menuElement.style.cssText = `--items-count: ${items.length};`
+        items.forEach((item, i) => item.style.cssText = `--i:${i}`)
+        toggleBtn.addEventListener('click', () => this.menu.classList.toggle('active'))
     }
 
-    open(menuId) {
-        this._menuCollection[menuId].classList.add('active')
+    open() {
+        this.menu.classList.add('active')
     }
 
-    close(menuId) {
-        this._menuCollection[menuId].classList.remove('active')
+    close() {
+        this.menu.classList.remove('active')
     }
 
-    getMenu(menuId) {
-        return this._menuCollection[menuId]
+    getMenu() {
+        return this.menu
     }
 }
